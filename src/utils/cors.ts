0e64d625054f1e4b43c70e9b644490028aff4588
Noah Lee <lee.noah@outlook.com>
2023-10-30 17:42:16 -0400

    Update cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,6 +1,6 @@
 const corsOptions = {
   origin:
-    process.env.NODE_ENV === "development" ? "*" : "https://polyaux.fly.dev",
+    process.env.NODE_ENV === "development" ? "*" : ["https://polyaux.fly.dev", "https://polyaux.com"],
 };
 
 export default corsOptions;
